14f74cedfa50ecc848d4dd33f0ff076be7e23505
Mr.Huang <yellowwee@hotmail.com>
2025-07-27 00:05:33 +0800

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,7 +24,7 @@ export default function App() {
   }>({
     apiUrl: import.meta.env.DEV
       ? "http://localhost:2024"
-      : "http://localhsot:8123",
+      : "http://localhost:8123",
     assistantId: "agent",
     messagesKey: "messages",
     onUpdateEvent: (event: any) => {
