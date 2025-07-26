a6b79ffeba8f0a27cb7591b5f3b216e49b1f3e51
Mr.Huang <yellowwee@hotmail.com>
2025-07-26 23:45:26 +0800

    修改后端端口

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,10 +13,11 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
     proxy: {
       // Proxy API requests to the backend server
       "/api": {
-        target: "http://192.168.8.53:8000", // Default backend address
+        target: "http://192.168.8.53:8123", // Default backend address
         changeOrigin: true,
         // Optionally rewrite path if needed (e.g., remove /api prefix if backend doesn't expect it)
         // rewrite: (path) => path.replace(/^\/api/, ''),
